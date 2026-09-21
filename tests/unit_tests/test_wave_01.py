6791da5ebcdcc22662d72958fd3e03ab42e83086
younnlei <younl.eain@gmail.com>
2026-09-22 04:14:47 +0630

    wave1: complete test and file, all tests passed
    Co-authored-by: Divya Palusa <divyapalusa6@gmail.com>

--- tests/unit_tests/test_wave_01.py
+++ tests/unit_tests/test_wave_01.py
@@ -2,12 +2,12 @@
 import pytest
 from swap_meet.vendor import Vendor
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_vendor_has_inventory():
     vendor = Vendor()
     assert len(vendor.inventory) == 0
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_vendor_takes_optional_inventory():
     inventory = ["a", "b", "c"]
     vendor = Vendor(inventory=inventory)
@@ -16,7 +16,7 @@ def test_vendor_takes_optional_inventory():
     assert "b" in vendor.inventory
     assert "c" in vendor.inventory
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_adding_to_inventory():
     vendor = Vendor()
     item = "new item"
@@ -27,7 +27,7 @@ def test_adding_to_inventory():
     assert item in vendor.inventory
     assert result == item
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_removing_from_inventory_returns_item():
     item = "item to remove"
     vendor = Vendor(
@@ -40,7 +40,7 @@ def test_removing_from_inventory_returns_item():
     assert item not in vendor.inventory
     assert result == item
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_removing_not_found_is_none():
     item = "item to remove"
     vendor = Vendor(
@@ -49,7 +49,8 @@ def test_removing_not_found_is_none():
 
     result = vendor.remove(item)
 
-    raise Exception("Complete this test according to comments below.")
+    #raise Exception("Complete this test according to comments below.")
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
     # *********************************************************************
+    assert result is False
