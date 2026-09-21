ab8ec145ebaf7e2aef0dac37c079333b9bab6c3b
younnlei <younl.eain@gmail.com>
2026-09-22 04:08:46 +0630

    wave1: complete test and file, all tests passed
    Co-authored-by: Divya Palusa divyapalusa6@gmail.com

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,20 @@
 class Vendor:
-    pass
+    """ mangaing a vendor's inventory of items.
+    attributes: inventory(list), default empty list.
+    method : add(item): adds an item to the inventory. 
+    reove (item) from the inventory. """
+
+    def __init__(self, inventory=None):
+        if not inventory:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+    def add(self,item):
+        self.inventory.append(item)
+        return item
+    def remove(self, item):
+
+        if item in self.inventory: 
+            self.inventory.remove(item)
+            return item
+        return False
