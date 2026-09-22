0bb4fbe50b7014195e230cc742f3e1155ddd31bb
younnlei <younl.eain@gmail.com>
2026-09-22 21:00:41 +0630

    wave5: add condition attribute to Item

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -5,11 +5,13 @@ class Item:
     attributes: id(int)
     method: get_category()
     """
-    def __init__(self,id=None):
+    def __init__(self,id=None, condition=0):
         if not id:
             self.id = uuid.uuid4().int
         else:
             self.id = id
+
+        self.condition = condition
 
     def get_category(self):
         """ returns the name of the class instance as  self.__class__.__name__      """
