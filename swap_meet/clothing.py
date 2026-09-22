aca39b69a0733987d50f456706e87d3dff1dff1f
Divya Palusa <divyapalusa6@gmail.com>
2026-09-22 13:06:18 -0700

    wave 5, clothing class updated

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,13 @@
-class Clothing:
-    pass
+from swap_meet.item import item
+class Clothing(item):
+
+    def __init__(self, id=None,condition=0, fabric="Unknown"):
+        """ clothig class,sub class of Item 
+        Attributes :fabric(str), default = "Unknown"
+        Methods: get_category() returns "Clothing"; __str__() decribes fabric.
+        """
+        super().__init__(id, condition)
+        self.fabric = fabric
+
+    def get_category(self):
+        return "Clothing"
--- tests/unit_tests/test_wave_05.py
+++ tests/unit_tests/test_wave_05.py
@@ -7,17 +7,17 @@ TEST_CUSTOM_ID = 12345
 
 # ~~~~~ Clothing Tests ~~~~~
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_clothing_has_default_uuid_length_id():
     clothing = Clothing()
     check_for_default_uuid_length_id(clothing)
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_clothing_has_expected_category_and_custom_id():
     clothing = Clothing(id=TEST_CUSTOM_ID)
     check_category_and_custom_id(clothing, TEST_CUSTOM_ID, "Clothing")
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_clothing_has_expected_default_to_str():
     clothing = Clothing(id=TEST_CUSTOM_ID)
     expected_str = (
@@ -26,7 +26,7 @@ def test_clothing_has_expected_default_to_str():
     )
     assert str(clothing) == expected_str
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_clothing_has_expected_to_str_with_custom_fabric():
     clothing = Clothing(id=TEST_CUSTOM_ID, fabric="Pinstriped")
     expected_str = (
@@ -37,17 +37,17 @@ def test_clothing_has_expected_to_str_with_custom_fabric():
 
 # ~~~~~ Decor Tests ~~~~~
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_decor_has_default_uuid_length_id():
     decor = Decor()
     check_for_default_uuid_length_id(decor)
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_decor_has_expected_category_and_custom_id():
     decor = Decor(id=TEST_CUSTOM_ID)
     check_category_and_custom_id(decor, TEST_CUSTOM_ID, "Decor")
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_decor_has_expected_default_to_str():
     decor = Decor(id=TEST_CUSTOM_ID)
     expected_str = (
@@ -56,7 +56,7 @@ def test_decor_has_expected_default_to_str():
     )
     assert str(decor) == expected_str
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_decor_has_expected_to_str_with_custom_size():
     decor = Decor(id=TEST_CUSTOM_ID, width=3, length=12)
     expected_str = (
@@ -67,17 +67,17 @@ def test_decor_has_expected_to_str_with_custom_size():
 
 # ~~~~~ Electronics Tests ~~~~~
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_electronics_has_default_uuid_length_id():
     electronics = Electronics()
     check_for_default_uuid_length_id(electronics)
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_electronics_has_expected_category_and_custom_id():
     electronics = Electronics(id=TEST_CUSTOM_ID)
     check_category_and_custom_id(electronics, TEST_CUSTOM_ID, "Electronics")
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_electronics_has_expected_default_to_str():
     electronics = Electronics(id=TEST_CUSTOM_ID)
     expected_str = (
@@ -86,7 +86,7 @@ def test_electronics_has_expected_default_to_str():
     )
     assert str(electronics) == expected_str
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_electronics_has_expected_to_str_with_custom_type():
     electronics = Electronics(id=TEST_CUSTOM_ID, type="Mobile Phone")
     expected_str = (
@@ -98,7 +98,7 @@ def test_electronics_has_expected_to_str_with_custom_type():
 
 # ~~~~~ Item Tests ~~~~~
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_items_have_condition_as_float():
     items = [
         Clothing(condition=3.5),
@@ -108,7 +108,7 @@ def test_items_have_condition_as_float():
     for item in items:
         assert item.condition == pytest.approx(3.5)
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_items_have_condition_descriptions_that_are_the_same_regardless_of_type():
     items = [
         Clothing(condition=5),
